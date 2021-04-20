ac25a65d0b8493959df572cd1a621bd131019782
Mikita Shah <mshah2511@gmail.com>
2021-04-20 23:34:48 +1000

    Multi-screen navigation using segue Passing value from one ViewController to another

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Mikita Shah on 20/4/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue = "0.0"
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+    }
+    
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
